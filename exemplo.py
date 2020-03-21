066848b78844f2b6ab6868fbe07a3aa29a3ec767
VitorAdemir <62472839+VitorAdemir@users.noreply.github.com>
2020-03-21 18:39:34 -0300

    Add files via upload

---
+++ exemplo.py
@@ -0,0 +1,27 @@
+print("olá mundo!")
+print("outra mensagem")
+print("mensagem 3")
+# isto é um comentario
+#nova linha
+print("nova mensagem")
+"""
+comentarios 
+de muitas 
+linhas
+"""
+print("mensagem")
+#-*- c
+print("olá mundo")
+# -*- coding: utf-8 -*-
+print(2+2)
+variavel= "ola mundo"
+print(variavel)
+var1 =1 #variavel inteira
+var2 =1.1 # tipo flutuante
+var3 = "eu sou uma string" # tipo textual
+var4= True #verdadeiro
+var4= False #falso
+print(var1)
+print(var2)
+print(var3)
+print(var4)
---
+++ ola mundo.py
@@ -0,0 +1,14 @@
+print("olá mundo!")
+print("outra mensagem")
+print("mensagem 3")
+# isto é um comentario
+#nova linha
+print("nova mensagem")
+"""
+comentarios 
+de muitas 
+linhas
+"""
+print("mensagem")
+-*- coding: utf-8
+print("olá mundo")
